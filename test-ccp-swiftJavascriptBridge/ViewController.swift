61af017cb646391da540c47c865978ad00deb56b
davidkneely <deeniri@gmail.com>
2016-12-30 07:23:19 -1000

    Initial Commit

---
+++ test-ccp-swiftJavascriptBridge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test-ccp-swiftJavascriptBridge
+//
+//  Created by pair on 12/30/16.
+//  Copyright © 2016 pair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
